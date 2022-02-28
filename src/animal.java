609478a3254982f1d14406220342afa43a0e5365
purdescupatricia19 <purdescupatricia19@stud.ase.ro>
2022-02-28 23:01:30 +0200

    sfarsitul seminarului 1

---
+++ src/animal.java
@@ -0,0 +1,17 @@
+public abstract class Animal {
+    private String nume;
+
+    public Animal(String nume) {
+        this.nume = nume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public abstract void mananca(String tipMancare);
+}
---
+++ src/girafa.java
@@ -0,0 +1,11 @@
+public class Girafa extends Animal {
+
+    public Girafa(String nume) {
+        super(nume);
+    }
+
+    @Override
+    public void mananca(String tipMancare) {
+        System.out.println("Girafa " + getNume() + "mananca " + tipMancare);
+    }
+}
